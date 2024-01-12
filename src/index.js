146ade9e8ef0253d99930f45057edfe7905f814d
JonatanDrysen <jojodrysen@gmail.com>
2024-01-12 12:02:35 +0100

    add task 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let nr1
 let nr2
+let operand
 
 function getFirstNr() {
     const nr1Prompt = Number(window.prompt("Enter the first number.", "0"))
@@ -9,4 +10,17 @@ function getFirstNr() {
 function getSecondNr() {
     const nr2Prompt = Number(window.prompt("Enter the second number.", "0"))
     nr2 = nr2Prompt
+}
+
+function checkOperand(operandPrompt) {
+    if(operandPrompt === "+" || "-" || "*" || "/") {
+        return true
+    } else {
+        return false
+    }
+}
+
+function getOperand() {
+    const operandPrompt = window.prompt("Enter the operand.")
+    checkOperand(operandPrompt) ? operand = operandPrompt : window.alert("Enter one of these operands: + - * /")
 }
