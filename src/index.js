4463957a62186d9f43c02374a78a2ccf950b59df
JonatanDrysen <jojodrysen@gmail.com>
2024-01-12 11:55:32 +0100

    add task 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 let nr1
+let nr2
 
 function getFirstNr() {
     const nr1Prompt = Number(window.prompt("Enter the first number.", "0"))
     nr1 = nr1Prompt
 }
 
+function getSecondNr() {
+    const nr2Prompt = Number(window.prompt("Enter the second number.", "0"))
+    nr2 = nr2Prompt
+}
